b8df0de831bec774a05c04df4af770d1ca41fef0
Jonah Duckles <jonah@duckles.org>
2020-08-27 12:29:10 +1200

    fixed comment

--- twitter_analysis.R
+++ twitter_analysis.R
@@ -7,7 +7,7 @@ frequent_words <- function(query, n=500, more_than=5, lang=NULL) {
 
   tweets <- search_tweets(query, n=500, lang=lang) 
   
-  other_stops = c('is', 'https', 't.co') # removing al from et al.
+  other_stops = c('is', 'https', 't.co') # removing https and t.co from tweets
   stops <- rbind(stop_words,data.frame(word=other_stops,lexicon="custom")) %>% transmute(text=word)
 
   text_tokens <- tweets %>% select(text) %>% unnest_tokens(text, text) %>% anti_join(stops) 
